923d9563d96e9d4a23325dac32da99c372872aca
danie <daniel.ek@skola.botkyrka.se>
2024-10-24 14:46:45 +0200

    fixed some code structure

--- public/contact.js
+++ public/contact.js
@@ -1,13 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('DOM fully loaded and parsed');
-
   const form = document.getElementById('contactForm');
   
   form.addEventListener('submit', function (e) {
     e.preventDefault();
-
-    console.log('Form submitted');
-
     emailjs.sendForm('service_v14l87c', 'template_326of3s', form)
       .then(function (response) {
         console.log('SUCCESS!', response.status, response.text);
